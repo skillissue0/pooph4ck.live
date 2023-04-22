d49b25d806629e53dc902a868d59eae34dbd444b
voltou <121342575+skillissue0@users.noreply.github.com>
2023-04-23 01:13:14 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,11 @@
+local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
+
+if game.PlaceId == (12888107609) then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/skillissue0/poopyhub/main/wallpunchsim.lua"))()
+else
+Notification:Notify(
+    {Title = "poopyHub", Description = "This script not supported for this game,Sorry!"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "default"}
+)
+end
