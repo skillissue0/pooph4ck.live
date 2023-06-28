5a26252f8e7ada953645da44bbbbb61fb1794a97
voltou <121342575+skillissue0@users.noreply.github.com>
2023-06-28 14:42:24 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,11 +1,13 @@
 local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
 local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
 
-if game.PlaceId == 12888107609 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/skillissue0/poopyhub/main/wallpunchsim.lua"))()
-if game.PlaceId == 4787629450 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/skillissue0/pooph4ck.live/main/Games/wallpunchsim.lua"))()
+if game.PlaceId == (12888107609) then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/skillissue0/pooph4ck.live/main/Games/wallpunchsim.lua"))()
+
+if game.PlaceId == (2866967438) then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/skillissue0/pooph4ck.live/main/Games/fishingsim.lua"))()
 end
+
 else
 Notification:Notify(
     {Title = "pooph4ck.live/notsupport", Description = "This script not supported for this game,Sorry!"},
