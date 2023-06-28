74d7f0be0018c2e00fd3d70f0232f86a483a193c
voltou <121342575+skillissue0@users.noreply.github.com>
2023-06-28 13:55:18 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,7 +5,7 @@ if game.PlaceId == (12888107609) then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/skillissue0/poopyhub/main/wallpunchsim.lua"))()
 else
 Notification:Notify(
-    {Title = "pooph4ck.live", Description = "This script not supported for this game,Sorry!"},
+    {Title = "pooph4ck.live/notsupport", Description = "This script not supported for this game,Sorry!"},
     {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "default"}
 )
 end
